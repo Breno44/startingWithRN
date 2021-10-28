4eb4b09b771dc51ead328b54b45148db82ecf0ce
Breno <bhfs.2908@gmail.com>
2021-10-28 18:02:33 -0300

    feat:finish

--- src/components/Form/result/styles.js
+++ src/components/Form/result/styles.js
@@ -4,7 +4,7 @@ export const styles = StyleSheet.create({
   resultImc: {
     flex: 1,
     marginTop: 15,
-    paddingTop: 60,
+    paddingTop: 35,
     borderRadius: 50,
     alignItems: "center",
     width: "100%",
@@ -18,5 +18,21 @@ export const styles = StyleSheet.create({
     fontSize: 48,
     color: "#ff0000",
     fontWeight: "bold",
+  },
+  boxShareButton: {
+    width: "100%",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  shared: {
+    backgroundColor: "#1877f2",
+    borderRadius: 50,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  sharedText: {
+    color: "#ffff",
+    fontWeight: "bold",
+    paddingHorizontal: 30,
   },
 });
